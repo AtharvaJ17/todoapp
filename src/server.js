4a1be6799ed5809b2669fe9b07a5a36312dbbd0f
AtharvaJ17 <atharva17171@gmail.com>
2026-09-18 15:24:23 +0530

    port fix

--- src/server.js
+++ src/server.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes)
 app.use('/todos', authMiddleware, todoRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server has started on port: ${PORT}`)
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
